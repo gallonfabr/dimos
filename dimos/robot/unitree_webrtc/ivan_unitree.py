c8ae9d6c16344107feaf125f0c6f9400fdf23299
lesh <lesh@sysphere.org>
2025-12-14 07:37:00 +0000

    simpler unitree run
    Former-commit-id: 1ecdf6759d5fa51422c29419e37ea70620441cc3 [formerly a7d39f323179e9b90b6a6979a47272c25ab8008e] [formerly 0203ecbfeb118bf61926e3cc691576b8051712fe [formerly ad222272cbfc2513972092dc5c6d905244eaeb02]]
    Former-commit-id: 1187129e7ebe80ddccb1ea1a96463d5eedd7aedf
    Former-commit-id: 7705174955bbec55a9165c657e12d5d074b90d1a

---
+++ dimos/robot/unitree_webrtc/ivan_unitree.py
@@ -0,0 +1,74 @@
+# Copyright 2025 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+import time
+from typing import Optional
+
+from dimos_lcm.sensor_msgs import CameraInfo
+
+from dimos.core import LCMTransport, start
+from dimos.msgs.geometry_msgs import PoseStamped, Quaternion, Transform, Vector3
+from dimos.msgs.sensor_msgs import Image
+from dimos.protocol.pubsub import lcm
+from dimos.robot.foxglove_bridge import FoxgloveBridge
+from dimos.robot.unitree_webrtc.connection import UnitreeWebRTCConnection
+from dimos.robot.unitree_webrtc.connectionModule import ConnectionModule, FakeRTC
+from dimos.utils.logging_config import setup_logger
+
+logger = setup_logger("dimos.robot.unitree_webrtc.unitree_go2", level=logging.INFO)
+
+from dimos.robot.unitree_webrtc.type.lidar import LidarMessage
+
+
+class UnitreeGo2:
+    def __init__(
+        self,
+        ip: str,
+        connection_type: Optional[str] = "fake",
+    ):
+        dimos = start(3)
+
+        foxglove_bridge = dimos.deploy(FoxgloveBridge)
+        foxglove_bridge.start()
+
+        connection = dimos.deploy(ConnectionModule, ip, connection_type)
+        connection.lidar.transport = LCMTransport("/lidar", LidarMessage)
+        connection.odom.transport = LCMTransport("/odom", PoseStamped)
+        connection.video.transport = LCMTransport("/image", Image)
+        connection.movecmd.transport = LCMTransport("/cmd_vel", Vector3)
+        connection.camera_info.transport = LCMTransport("/camera_info", CameraInfo)
+
+        connection.start()
+
+    def stop(): ...
+
+
+def main():
+    lcm.autoconf()
+    robot = UnitreeGo2(
+        ip=os.getenv("ROBOT_IP"), connection_type=os.getenv("CONNECTION_TYPE", "fake")
+    )
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        robot.stop()
+        logger.info("Shutting down...")
+
+
+if __name__ == "__main__":
+    main()
